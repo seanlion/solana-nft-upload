f6a4d9e0c583f2eaa1da547b94982c1f626f1c3f
Ubuntu <ubuntu@ip-172-31-34-14.ap-northeast-2.compute.internal>
2022-03-17 15:42:33 +0000

    fix: add cors library

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import cors from 'cors';
+
 const app =express();
 import {routesFunc} from './routes.js';
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 const _id = process.env.KEYPAIR;
 routesFunc(app,_id);
